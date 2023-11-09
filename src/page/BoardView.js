a02d73f8452d573378a9fbc800bc19f03ef316f1
Jae Hee <travel237corp@naver.com>
2023-11-09 16:40:06 +0900

    토스트 , 네비게이트 등 삭제기능 만들기

--- src/page/BoardView.js
+++ src/page/BoardView.js
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { Box, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 
 export function BoardView() {
   const [board, setBoard] = useState(null);
   const { id } = useParams();
+
+  //
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -17,14 +40,62 @@ export function BoardView() {
     return <Spinner />;
   }
 
+  function handleDelete() {
+    axios
+      .delete("/api/board/remove/" + id)
+      .then((response) => {
+        toast({ description: id + "가 삭제되었습니다.", status: "success" });
+        navigate("/");
+      })
+      .catch((error) => {
+        toast({
+          description: "삭제중 문제가 발생했습니다.",
+          status: "error",
+        });
+      })
+      .finally(() => console.log("끝"));
+  }
+
   return (
     <Box>
-      <h1>글보기</h1>
-      <p>번호:{board.id}</p>
-      <p>제목:{board.title}</p>
-      <p>본문:{board.content}</p>
-      <p>작성자:{board.writer}</p>
-      <p>작성일지:{board.inserted}</p>
+      <h1>{board.id}번 글 보기</h1>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input value={board.title} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Textarea value={board.content} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성자</FormLabel>
+        <Input value={board.writer} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성일시</FormLabel>
+        <Input value={board.inserted} readOnly />
+      </FormControl>
+      <Button colorScheme="purple"> 수정</Button>
+      <Button colorScheme="red" onClick={onOpen}>
+        {" "}
+        삭제
+      </Button>
+      {/* 삭제 모달  새창 나와서 삭제 마지막확인 하는작업 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody> 삭제 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleDelete} colorScheme="red">
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
