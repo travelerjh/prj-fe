7374a48e27d6d5cce4810ffd88c0d2faf6ba5708
Jae Hee <travel237corp@naver.com>
2023-11-09 15:29:51 +0900

    각 행 클릭시 게시글 보기 링크로 이동

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -4,15 +4,20 @@ import {
   Button,
   Spinner,
   Table,
+  Tbody,
   Td,
   Th,
   Thead,
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
+
+  //내비게이션 추가
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -33,16 +38,25 @@ export function BoardList() {
               <Th>at</Th>
             </Tr>
           </Thead>
-          {boardList || <Spinner />}
-          {boardList &&
-            boardList.map((board) => (
-              <Tr>
-                <Td>{board.id}</Td>
-                <Td>{board.title}</Td>
-                <Td>{board.writer}</Td>
-                <Td>{board.inserted}</Td>
-              </Tr>
-            ))}
+          <Tbody>
+            {boardList === null ? (
+              <Spinner />
+            ) : (
+              boardList &&
+              boardList.map((board) => (
+                <Tr
+                  _hover={{ cursor: "pointer" }}
+                  key={board.id}
+                  onClick={() => navigate("/borad/" + board.id)}
+                >
+                  <Td>{board.id}</Td>
+                  <Td>{board.title}</Td>
+                  <Td>{board.writer}</Td>
+                  {/*<Td>{board.inserted}</Td>*/}
+                </Tr>
+              ))
+            )}
+          </Tbody>
         </Table>
       </Box>
     </Box>
