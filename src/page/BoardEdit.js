277606d9453852da61db213e515e25172591e71c
Jae Hee <travel237corp@naver.com>
2023-11-10 09:40:49 +0900

    boardEdit immer활용해서 title content writer 바꾸기

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -9,6 +9,7 @@ import {
 import { useParams } from "react-router-dom";
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useImmer } from "use-immer";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
@@ -26,6 +27,27 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  // 타이틀 바꾸는 역활
+  function handleChangeT(e) {
+    updateBoard((draft) => {
+      draft.title = e.target.value;
+    });
+  }
+
+  // 컨텐츠 바꾸는 역활
+  function handleChangeC(e) {
+    updateBoard((draft) => {
+      draft.content = e.target.value;
+    });
+  }
+
+  //함수마다 이름이달라야 한다.
+  function handleChangeW(e) {
+    updateBoard((draft) => {
+      draft.writer = e.target.value;
+    });
+  }
+
   return (
     <>
       <Box>
@@ -33,15 +55,15 @@ export function BoardEdit() {
         <h1>{board.id}번 글 보기</h1>
         <FormControl>
           <FormLabel>제목</FormLabel>
-          <Input value={board.title} />
+          <Input value={board.title} onChange={handleChangeT} />
         </FormControl>
         <FormControl>
           <FormLabel>본문</FormLabel>
-          <Textarea value={board.content} />
+          <Textarea value={board.content} onChange={handleChangeC} />
         </FormControl>
         <FormControl>
           <FormLabel>작성자</FormLabel>
-          <Input value={board.writer} />
+          <Input value={board.writer} onChange={handleChangeW} />
         </FormControl>
       </Box>
     </>
